59487ba69dfd2112818ac28d2e2ceb60996cb93a
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2015-04-30 13:06:15 -0400

    Command-t mapping to <l>to/tf/tb.
    Also added setup for Command-T.
    Also, added quick push with <l>gp.
    Also, added quick pull with <l>gu.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+# Initialize Vundle
 git clone git@github.com:gmarik/Vundle.vim.git bundle/Vundle.vim
 vim +PluginInstall +qall
+
+# Compile Command-t
+cd bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
