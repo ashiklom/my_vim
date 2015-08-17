896244a848928dd7b5fec702cad7b92714fd471c
Alexey Shiklomanov (LVM64) <alexey.shiklomanov@gmail.com>
2015-08-17 13:00:38 -0400

    Updated Rsetup

--- Rsetup.R
+++ Rsetup.R
@@ -4,13 +4,9 @@ for(p in pkgs)
     if(!(p %in% installed.packages())) install.packages(p)
 library(devtools)
 install_github('jalvesaq/nvimcom')
-download.file("http://www.lepem.ufc.br/jaa/vimr/colorout_1.1-0.tar.gz", destfile = "colorout_1.1-0.tar.gz")
-install.packages("colorout_1.1-0.tar.gz", type="source", repos=NULL)
-
-#Paste the following into ~/.Rprofile:
-#if(interactive()){
-       #library(colorout) # For colorized output in terminal emulators
-       #library(setwidth) # For automatic update of options('width')
-       #options(nvimcom.verbose = 1) # To know nvimcom was successfully loaded
-       #library(nvimcom)
-#}
+colorout.fname <- "colorout_1.1-0.tar.gz"
+colorout.link <- paste0("http://www.lepem.ufc.br/jaa/vimr/",
+                        colorout.fname)
+download.file(colorout.link, destfile = "colorout.tar.gz")
+install.packages("colorout.tar.gz", type="source", repos=NULL)
+file.remove("colorout.tar.gz")
