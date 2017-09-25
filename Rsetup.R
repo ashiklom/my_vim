7708637880b3cfd014da6f70250bd488c70f127d
Alexey Shiklomanov <ashiklom@bu.edu>
2017-09-25 11:30:56 -0400

    Deprecate old setup scripts

--- Rsetup.R
+++
@@ -1,7 +0,0 @@
-## Install dependencies for Nvim R package
-pkgs <- c("devtools", "setwidth")
-for(p in pkgs)
-    if(!(p %in% installed.packages())) install.packages(p)
-library(devtools)
-install_github('jalvesaq/nvimcom')
-install_github('jalvesaq/colorout')
--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Initialize Vundle and install packages
-nvim +PlugInstall
-
-# Install R plugin dependencies
-Rscript Rsetup.R
-rm colorout_*
